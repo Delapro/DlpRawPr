a55e01c4b2731b7c457508921dd2d4d867464b71
Delapro <Delapro>
2016-11-08 16:24:49 +0100

    Initialfassung

---
+++ DlpRawPr/Program.cs
@@ -0,0 +1,121 @@
+﻿/*
+ * 
+ *  Grundlage diese Programms war der KB-Artikel: http://support.microsoft.com/kb/322091/en-us
+ *  Ähnlicher Artikel für VB6: http://support.microsoft.com/kb/154078/en-us
+ *  
+ *  SimpleMapi Unterstützung: http://www.codeproject.com/KB/IP/simplemapidotnet.aspx
+ * 
+ * *
+ * * Testcommandline: h:\d\grafik\hp.out "FRITZfax Drucker"
+ * *                  C:\delapro\laser\hp.out "\\server\hp2300 EX"
+ * 
+ *    (C) 2009-2016 by easy - innovative software
+ * */
+
+using System;
+using System.Text;
+using System.Drawing.Printing;
+using System.Windows.Forms;
+
+namespace DlpRawPr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool run = true;
+            bool showhelp = false;
+            bool appendformfeed = false;
+            string parameter = "";
+
+            if (args.Length == 0)
+                showhelp = true;
+            else
+                if (args[0] == "/?" || args[0] == "-?")
+                    showhelp = true;
+                else
+                {
+                    if (args.Length == 1)
+                    {
+                        if (args[0].ToUpper() == @"/DRUCKERAUSWAHL")
+                        {
+                            ShowPrinterDrivers();
+                            run = false;
+                        }
+                        parameter = "";
+                    }
+                    else
+                    {
+                        parameter = args[1];
+                        // folgende Zeile wurde wieder rausgenommen, weil es im Zusammenhang mit dem Delapro und leeren
+                        // Batchparametern %4 %5 %6 %7 usw. Probleme gab und diese obwohl leer als Parameter verstanden
+                        // wurden
+                        // if (args.Length > 3)
+                        //    showhelp = true;
+                    }
+
+                    foreach (string para in args)
+                	{
+                        if (para.ToUpper () == "/FORMFEED")
+                        {
+                            appendformfeed = true;
+                        }
+	                }
+                    
+                }
+
+            if (!showhelp)
+            {
+                if (run)
+                {
+                    PrintRaw(args[0], parameter, appendformfeed);
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                }
+                
+            }
+            else
+            {
+                System.Reflection.AssemblyProductAttribute productAttribute;
+                System.Reflection.AssemblyVersionAttribute versionAttribute;
+
+                productAttribute = (System.Reflection.AssemblyProductAttribute)Attribute.GetCustomAttribute(System.Reflection.Assembly.GetExecutingAssembly(), typeof(System.Reflection.AssemblyProductAttribute));
+                versionAttribute = (System.Reflection.AssemblyVersionAttribute) Attribute.GetCustomAttribute (System.Reflection.Assembly.GetExecutingAssembly (), typeof (System.Reflection.AssemblyVersionAttribute));
+                 
+                //Console.WriteLine ("{0}",  System.Reflection.Assembly.GetExecutingAssembly().GetName().Name);
+
+                Console.WriteLine("Aufruf : {0} <Drucker-RawDatei> [<Windowsdruckertreiber>] [/FORMFEED]", Application.ProductName);
+                Console.WriteLine("Version: {0}", Application.ProductVersion);
+                Console.WriteLine("Erlaubt das Schreiben von Daten direkt in der vom Drucker verstandenen Sprache.");
+                Console.WriteLine("Versteht ein Druckertreiber das verwendete Format nicht, kann es auch sein,");
+                Console.WriteLine("dass die Ausgabe einfach verschluckt wird. (pdffactory, Fritzfax usw.)");
+                Console.WriteLine("");
+                Console.WriteLine("Durch Aufruf mit dem Parameter /DRUCKERAUSWAHL werden die installierten Drucker angezeigt.");
+            }
+        }
+
+        private static void ShowPrinterDrivers()
+        {
+            foreach (string printerName in PrinterSettings.InstalledPrinters)
+            {
+                Console.WriteLine ("{0}", printerName);
+            }
+        }
+
+        private static void PrintRaw(string printerRawDataFile, string printerDriver, bool appendFormfeed)
+        {
+            var ps = new PrinterSettings ();
+            var encoding = Encoding.ASCII;
+
+            if (printerDriver == String.Empty || printerDriver.ToUpper() == "UNBEKANNT" || printerDriver.ToUpper () == "STANDARD")
+                printerDriver = ps.PrinterName;
+            
+            RawPrinterHelper.SendFileToPrinter(printerDriver, printerRawDataFile, appendFormfeed);
+
+            // Der Weg über den Streamreader sorgt trotz Encoding für verpuzzelte Umlaute:
+            //var sr = new StreamReader(printerRawDataFile, System.Text.Encoding.GetEncoding(850)); // encoding);
+            
+            //RawPrinterHelper.SendStringToPrinter(printerDriver, sr.ReadToEnd(), appendFormfeed);
+        }
+    }
+}
---
+++ DlpRawPr/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DlpRawPr - Delapro-Hilfsprogramm zum direkten Ansprechen von Druckern unter Windows.")]
+[assembly: AssemblyDescription("Delapro-Hilfsprogramm zum direkten Ansprechen von Druckern unter Windows.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("easy innovative software")]
+[assembly: AssemblyProduct("DlpRawPr")]
+[assembly: AssemblyCopyright("Copyright © 2009-2016 by easy innovative software")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0ad12870-a5a0-4349-a36f-2a924622d92c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
